1668487d7830e360f4dde14c7f9c144662b1a222
hwiju yeom <yeom456@gmail.com>
2023-11-03 23:57:09 +0900

    Feat: 로또 번호 생성 util class

---
+++ src/main/java/lotto/utils/LottoNumbersGenerator.java
@@ -0,0 +1,13 @@
+package lotto.utils;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class LottoNumbersGenerator {
+
+    public static List<Integer> generateLottoNumbers(){
+        return Randoms.pickUniqueNumbersInRange(1, 45,6);
+    }
+
+}
