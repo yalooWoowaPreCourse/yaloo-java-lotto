564b3689be5a93ecb50c9d36923be0d4aa7611a9
hwiju yeom <yeom456@gmail.com>
2023-11-06 19:03:06 +0900

    Feat: 입력과 출력에 사용하는 메시지를 열거형 상수로 사용

--- src/main/java/lotto/view/LottoMessage.java
+++ src/main/java/lotto/view/LottoMessage.java
@@ -1,9 +1,22 @@
 package lotto.view;
 
 public enum LottoMessage {
+
+    //상수("속성값"), 
     PURCHASE_PRICE_INPUT_MESSAGE("구입금액을 입력해 주세요."),
+    PURCAHSE_AMOUNT_OUTPUT_MESSAGE("%d개를 구매했습니다.\n"),
     WINNING_NUMBER_INPUT_MESSAGE("당첨 번호를 입력해 주세요."),
-    BONUS_NUMBER_INPUT_MESSAGE("보너스 번호를 입력해 주세요.");
+    BONUS_NUMBER_INPUT_MESSAGE("보너스 번호를 입력해 주세요."),
+    TOTAL_RETURN_RATE_OUTPUT_MESSAGE("총 수익률은 %d%%입니다"),
+
+    WINNING_PRIZE_RESULT_OUTPUT_MESSAGE("당첨 통계\n" +
+            "--\n" +
+            "3개 일치 (5,000원) - d%개 " +
+            "4개 일치 (50,000원) - d%개 " +
+            "5개 일치 (1,500,000원) - d%개 " +
+            "5개 일치, 보너스 볼 일치 (30,000,000원) - d%개 " +
+            "6개 일치 (2,000,000,000원) - d%개");
+
 
     private String message;
 
