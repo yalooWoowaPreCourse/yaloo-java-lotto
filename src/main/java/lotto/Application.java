bc3f4d05484f546113c6f4aaad1af285dba7845d
hwiju yeom <yeom456@gmail.com>
2023-11-07 15:35:27 +0900

    Feat: 프로그램 구현 시작

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,28 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.domain.Lotto;
+import lotto.domain.LottoGame;
+import lotto.domain.LottoResultCalculator;
+import lotto.utils.LottoInputValidator;
+import lotto.view.LottoInputView;
+import lotto.view.LottoOutputView;
+
 public class Application {
     public static void main(String[] args) {
+
         // TODO: 프로그램 구현
+
+        Lotto lotto = new Lotto();
+        LottoInputView lottoInputView = LottoInputView.getInstance();
+        LottoOutputView lottoOutputView = new LottoOutputView();
+        LottoResultCalculator lottoResultCalculator = new LottoResultCalculator();
+        LottoInputValidator lottoInputValidator = new LottoInputValidator();
+
+        LottoController lottoController = new LottoController(lotto,lottoInputView ,lottoOutputView,
+               lottoResultCalculator,lottoInputValidator);
+
+        lottoController.start();
+
     }
 }
